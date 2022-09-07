6384aa7104c517e3df870621df6691bd54a7c7df
Glenn <glenou@yahoo.com>
2022-09-07 18:16:30 +0800

    car-data

--- src/components/Cars.vue
+++ src/components/Cars.vue
@@ -10,8 +10,43 @@
           <span>新能源汽车 5座</span>
         </p>
       </header>
-      <div class="car-content"></div>
-      <footer></footer>
+      <div class="car-content">
+        <div class="car-info">
+          <div>
+            <h3 class="car-license">粤 B745N8</h3>
+
+            <ul class="car-battery">
+              <li class="car-battery-item active"></li>
+              <li class="car-battery-item active"></li>
+              <li class="car-battery-item active"></li>
+              <li class="car-battery-item active"></li>
+              <li class="car-battery-item active"></li>
+              <li class="car-battery-item active"></li>
+              <li class="car-battery-item active"></li>
+              <li class="car-battery-item active"></li>
+              <li class="car-battery-item"></li>
+              <li class="car-battery-item"></li>
+              <p class="car-distance">
+                <sub>约</sub>
+                <strong> 600 </strong>
+                <sub>KM</sub>
+              </p>
+            </ul>
+          </div>
+        </div>
+
+        <img class="car-img" src="../assets/car-img.png" alt="" />
+      </div>
+
+      <footer>
+        <!-- <hr> -->
+        <a
+          href="https://www.bilibili.com/video/BV1Gz411z75G?p=15&vd_source=a6dcde83e717a44e76ae7c56de63a455"
+          class="park-link"
+          >某某停车场
+          <img src="../assets/right-arrow.png" alt="" class="right-arrow" />
+        </a>
+      </footer>
     </section>
     <span class="cars-title" @click="user">This is the Cars Data</span>
   </div>
@@ -38,13 +73,14 @@ export default {
 .cars-list {
   position: fixed;
   z-index: 12;
-  bottom: 200px;
+  bottom: 170px;
   left: 0;
   right: 0;
 }
 .cars-title {
 }
 .car-item {
+
   width: 400px;
   height: 150px;
   border-radius: 24px;
@@ -67,8 +103,71 @@ img {
 .car-name {
   font-size: 16px;
 }
-.car-attr{
+.car-attr {
   text-align: right;
-  opacity: .5;
+  opacity: 0.5;
 }
+.car-img {
+  float: right;
+  height: 80px;
+  width: 190px;
+}
+
+.car-content {
+  position: relative;
+  height: 70px;
+}
+.car-info {
+  display: flex;
+  position: absolute;
+  width: 100%;
+  height: 100px;
+  left: 0;
+  top: 0;
+  /* background-color: red; */
+}
+.car-license {
+  font-size: 18px;
+  padding-left: 8px;
+}
+.car-bettery {
+  padding-left: 8px;
+}
+.car-battery {
+  padding-left: 8px;
+}
+.car-battery-item {
+  display: inline-block;
+  width: 6px;
+  height: 20px;
+  border-radius: 100px;
+  background-color: #e1e0e1;
+  margin-right: 5px;
+}
+.active {
+  background-color: #009bf3;
+}
+.car-distance {
+  margin-left: 5px;
+  display: inline-block;
+}
+strong {
+  font-size: 22px;
+}
+footer {
+  padding-top: 8px;
+  padding-left: 15px;
+  display: inline-block;
+}
+.park-link {
+  position: relative;
+  display: block;
+  border-top: 1px solid #e1e0e1;
+  color: #b2b2b4;
+  opacity: 0.5;
+}
+.right-arrow {
+  float: right;
+}
+
 </style>
